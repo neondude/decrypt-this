86d04a31b6861ad9d1c959e1f2f2235e3f8c44a2
neondude <siddharth.nagrajann@gmail.com>
2025-09-24 00:14:55 +0530

    fix: set dynamic base path for Vite configuration and update GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [vue(), vueDevTools(), VitePWA()],
   resolve: {
     alias: {
